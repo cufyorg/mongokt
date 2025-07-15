39d8d567937162eb55e7e327eb13a492055cdb5b
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-07-15 08:32:40 +0300

    chore: more literal(...) overloads

--- module-mongokt/src/commonMain/kotlin/expr/_Literal.kt
+++ module-mongokt/src/commonMain/kotlin/expr/_Literal.kt
@@ -3,14 +3,10 @@
 
 package org.cufy.mongodb.expr
 
-import org.cufy.bson.BsonDocumentBlock
-import org.cufy.bson.BsonElement
-import org.cufy.bson.BsonMarker2
-import org.cufy.bson.by
+import org.cufy.bson.*
 import org.cufy.mongodb.`$literal`
 import org.cufy.mongodb.ExperimentalMongodbApi
-import org.cufy.mongodb.expr.Expr._Document
-import org.cufy.mongodb.expr.Expr._Element
+import org.cufy.mongodb.expr.Expr.*
 
 // https://www.mongodb.com/docs/manual/reference/operator/aggregation/#literal-expression-operator
 
@@ -20,6 +16,18 @@ import org.cufy.mongodb.expr.Expr._Element
 @BsonMarker2
 context(_: ExprScope)
 fun literal(value: BsonElement): Expr<_Element> =
+    Expr { `$literal` by value }
+
+/** https://www.mongodb.com/docs/manual/reference/operator/aggregation/literal/ */
+@BsonMarker2
+context(_: ExprScope)
+fun literal(value: BsonDocument): Expr<_Document> =
+    Expr { `$literal` by value }
+
+/** https://www.mongodb.com/docs/manual/reference/operator/aggregation/literal/ */
+@BsonMarker2
+context(_: ExprScope)
+fun literal(value: BsonArray): Expr<_Array> =
     Expr { `$literal` by value }
 
 /** https://www.mongodb.com/docs/manual/reference/operator/aggregation/literal/ */
